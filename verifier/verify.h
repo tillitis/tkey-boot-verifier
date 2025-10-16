5dbe8d99b17a60614a51b325befd961e37c54847
Mikael Ågren <mikael@tillitis.se>
2025-10-16 13:27:09 +0200

    Include tkey/syscall.h where enum reset_start is used

--- verifier/verify.h
+++ verifier/verify.h
@@ -5,6 +5,7 @@
 #define VERIFY_H
 
 #include <stdint.h>
+#include <tkey/syscall.h>
 
 int reset_if_verified(uint8_t pubkey[32], enum reset_start reset_type,
 		      uint8_t app_digest[32], uint8_t app_signature[64]);
