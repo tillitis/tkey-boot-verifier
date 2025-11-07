a26b0cae46900419d8490cd3ec9bf035bdd16aff
Mikael Ågren <mikael@tillitis.se>
2025-11-07 10:43:23 +0100

    test: Move all fakesys state to a common struct

--- test/platform/fakesys.c
+++ test/platform/fakesys.c
@@ -16,31 +16,31 @@
 
 #define APP_SLOT_1_SIZE (128 * 1024)
 
-struct digsig {
+struct state {
 	uint8_t digest[32];
 	uint8_t signature[64];
+	uint8_t app[APP_SLOT_1_SIZE];
 };
 
-struct digsig digsig = {0};
-uint8_t app_slot_1[APP_SLOT_1_SIZE] = {0};
+struct state state = {0};
 
 int sys_get_digsig(uint8_t digest[32], uint8_t signature[64])
 {
-	memcpy(digest, digsig.digest, sizeof(digsig.digest));
-	memcpy(signature, digsig.signature, sizeof(digsig.signature));
+	memcpy(digest, state.digest, sizeof(state.digest));
+	memcpy(signature, state.signature, sizeof(state.signature));
 
 	return 0;
 }
 
 void fakesys_set_digsig(uint8_t digest[32], uint8_t signature[64])
 {
-	memcpy(digsig.digest, digest, sizeof(digsig.digest));
-	memcpy(digsig.signature, signature, sizeof(digsig.signature));
+	memcpy(state.digest, digest, sizeof(state.digest));
+	memcpy(state.signature, signature, sizeof(state.signature));
 }
 
 int sys_preload_store(uint32_t offset, void *app, size_t len)
 {
-	if (offset >= sizeof(app_slot_1)) {
+	if (offset >= sizeof(state.app)) {
 		return -1;
 	}
 
@@ -48,17 +48,17 @@ int sys_preload_store(uint32_t offset, void *app, size_t len)
 		return -1;
 	}
 
-	if (len > sizeof(app_slot_1)) {
+	if (len > sizeof(state.app)) {
 		return -1;
 	}
 
-	_Static_assert(sizeof(app_slot_1) < (UINT32_MAX / 2), "avoid overflow");
-	if ((offset + len) > sizeof(app_slot_1)) {
+	_Static_assert(sizeof(state.app) < (UINT32_MAX / 2), "avoid overflow");
+	if ((offset + len) > sizeof(state.app)) {
 		return -1;
 	}
 
 	for (size_t i = 0; i < len; i++) {
-		app_slot_1[offset + i] &= ((uint8_t *)app)[i];
+		state.app[offset + i] &= ((uint8_t *)app)[i];
 	}
 
 	return 0;
@@ -66,7 +66,7 @@ int sys_preload_store(uint32_t offset, void *app, size_t len)
 
 void fakesys_preload_erase(void)
 {
-	memset(app_slot_1, 0xff, sizeof(app_slot_1));
+	memset(&state.app_size, 0xff, sizeof(state.app_size));
 }
 
 bool fakesys_preload_range_contains_ff(uint32_t start, uint32_t stop)
@@ -79,16 +79,16 @@ bool fakesys_preload_range_contains_ff(uint32_t start, uint32_t stop)
 		return false;
 	}
 
-	if ((start < 0) || (start >= sizeof(app_slot_1))) {
+	if ((start < 0) || (start >= sizeof(state.app))) {
 		return false;
 	}
 
-	if ((stop < 0) || (stop > sizeof(app_slot_1))) {
+	if ((stop < 0) || (stop > sizeof(state.app))) {
 		return false;
 	}
 
 	for (size_t i = start; i < stop; i++) {
-		if (app_slot_1[i] != 0xff) {
+		if (state.app[i] != 0xff) {
 			cm_print_error(
 			    "app slot differs at offset: %lu, expected: "
 			    "0xff, got: %u\n",
@@ -103,23 +103,23 @@ bool fakesys_preload_range_contains_ff(uint32_t start, uint32_t stop)
 bool fakesys_preload_range_contains_data(uint32_t offset, void *data,
 					 size_t data_len)
 {
-	if (data_len > sizeof(app_slot_1)) {
+	if (data_len > sizeof(state.app)) {
 		return false;
 	}
 
-	if (offset > sizeof(app_slot_1)) {
+	if (offset > sizeof(state.app)) {
 		return false;
 	}
 
-	_Static_assert(sizeof(app_slot_1) < (UINT32_MAX / 2), "avoid overflow");
-	if ((offset + data_len) > sizeof(app_slot_1)) {
+	_Static_assert(sizeof(state.app) < (UINT32_MAX / 2), "avoid overflow");
+	if ((offset + data_len) > sizeof(state.app)) {
 		return false;
 	}
 
 	for (size_t i = offset; i < data_len; i++) {
 		uint8_t expected = *(((uint8_t *)data) + i);
 
-		if (app_slot_1[i] != expected) {
+		if (state.app[i] != expected) {
 			cm_print_error(
 			    "app slot differs at offset: %lu, expected: "
 			    "%u, got: %u\n",
