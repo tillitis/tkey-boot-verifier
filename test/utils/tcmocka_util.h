96d23cc0729dc38a9638e2fd611fe0ccd9bb957c
Mikael Ågren <mikael@tillitis.se>
2025-11-07 10:44:30 +0100

    test: Fix formatting
    Ran make -C test fmt

--- test/utils/tcmocka_util.h
+++ test/utils/tcmocka_util.h
@@ -4,6 +4,8 @@
 #ifndef TCMOCKA_UTIL_H
 #define TCMOCKA_UTIL_H
 
-#define print_subtest(...) print_message("[  SUBTEST ]  "); print_message(__VA_ARGS__) 
+#define print_subtest(...)                                                     \
+	print_message("[  SUBTEST ]  ");                                       \
+	print_message(__VA_ARGS__)
 
 #endif
