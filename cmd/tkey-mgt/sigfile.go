c1b758b239d37d616ae6d4d4fa61d60503fe888f
Mikael Ågren <mikael@tillitis.se>
2025-11-17 10:54:39 +0100

    Tidy up imported sigfile.go
    - Remove write functions
    - Use `[ed25519.SignatureSize]bytes` instead of `signature` type

--- cmd/tkey-mgt/sigfile.go
+++ cmd/tkey-mgt/sigfile.go
@@ -4,11 +4,10 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
+	"crypto/ed25519"
 	"encoding/base64"
 	"encoding/binary"
-	"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -35,25 +34,8 @@ func readBase64(filename string) ([]byte, error) {
 	return data, nil
 }
 
-func readKey(filename string) (*pubKey, error) {
-	var pub pubKey
-
-	buf, err := readBase64(filename)
-	if err != nil {
-		return nil, fmt.Errorf("%w", err)
-	}
-
-	r := bytes.NewReader(buf)
-	err = binary.Read(r, binary.BigEndian, &pub)
-	if err != nil {
-		return nil, fmt.Errorf("%w", err)
-	}
-
-	return &pub, nil
-}
-
-func readSig(filename string) (*signature, error) {
-	var sig signature
+func readSig(filename string) (*[ed25519.SignatureSize]byte, error) {
+	var sig [ed25519.SignatureSize]byte
 
 	buf, err := readBase64(filename)
 	if err != nil {
@@ -67,74 +49,4 @@ func readSig(filename string) (*signature, error) {
 	}
 
 	return &sig, nil
-}
-
-// writeBase64 encodes data in base64 and writes it the file given in
-// filename. If overwrite is true it overwrites any existing file,
-// otherwise it returns an error.
-func writeBase64(filename string, data any, comment string, overwrite bool) error {
-	var buf bytes.Buffer
-
-	err := binary.Write(&buf, binary.BigEndian, data)
-	if err != nil {
-		return fmt.Errorf("%w", err)
-	}
-
-	b64 := base64.StdEncoding.EncodeToString(buf.Bytes())
-	b64 += "\n"
-
-	var f *os.File
-
-	f, err = os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0o666)
-	if err != nil {
-		if os.IsExist(err) && overwrite {
-			f, err = os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0o666)
-			if err != nil {
-				return fmt.Errorf("%w", err)
-			}
-		} else {
-			return fmt.Errorf("%w", err)
-		}
-	}
-
-	defer f.Close()
-
-	_, err = f.Write([]byte(fmt.Sprintf("untrusted comment: %s\n", comment)))
-	if err != nil {
-		return fmt.Errorf("%w", err)
-	}
-	_, err = f.Write([]byte(b64))
-	if err != nil {
-		return fmt.Errorf("%w", err)
-	}
-
-	return nil
-}
-
-// writeRetry writes the data in the file given in filename as base64.
-// If a file already exists it prompts interactively for permission to
-// overwrite the file.
-func writeRetry(filename string, data any, comment string) error {
-	err := writeBase64(filename, data, comment, false)
-	if os.IsExist(errors.Unwrap(err)) {
-		le.Printf("File %v exists. Overwrite [y/n]?", filename)
-		reader := bufio.NewReader(os.Stdin)
-		overWriteP, _ := reader.ReadString('\n')
-
-		// Trim space to normalize Windows line endings
-		overWriteP = strings.TrimSpace(overWriteP)
-
-		if overWriteP == "y" {
-			err = writeBase64(filename, data, comment, true)
-		} else {
-			le.Printf("Aborted\n")
-			os.Exit(1)
-		}
-	}
-
-	if !os.IsExist(errors.Unwrap(err)) && err != nil {
-		return fmt.Errorf("%w", err)
-	}
-
-	return nil
 }
