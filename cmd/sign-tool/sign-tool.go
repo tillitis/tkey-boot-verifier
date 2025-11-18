49457d79ac71ca602bc015d3f979e52d4a119c08
Mikael Ågren <mikael@tillitis.se>
2025-11-18 11:18:11 +0100

    client: Add sign-tool utility to sign files with blake2s+ed25519
    file.go imported from:
    https://github.com/tillitis/tkey-sign-cli.git/cmd/tkey-sign/file.go
    main@557bced1fe920b23164302bbf71c01eeb5ef9834.

---
+++ cmd/sign-tool/sign-tool.go
@@ -0,0 +1,97 @@
+// SPDX-FileCopyrightText: 2025 Tillitis AB <tillitis.se>
+// SPDX-License-Identifier: BSD-2-Clause
+
+package main
+
+import (
+	"crypto/ed25519"
+	_ "embed"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"golang.org/x/crypto/blake2s"
+)
+
+// Use when printing err/diag msgs
+var le = log.New(os.Stderr, "", 0)
+
+func usage() {
+	fmt.Fprintf(flag.CommandLine.Output(), "%s -m FILE -s seckey\n\n", os.Args[0])
+	fmt.Fprintf(flag.CommandLine.Output(), "Sign message in FILE and write the result to file.sig.\n")
+	fmt.Fprintf(flag.CommandLine.Output(), "Signatures are produced by Ed25519-signing the Blake2s digest of message.\n\n")
+	flag.PrintDefaults()
+}
+
+type signature struct {
+	Alg    [2]byte
+	KeyNum [8]byte
+	Sig    [64]byte
+}
+
+func main() {
+	messagePath := flag.String("m", "", "File containing message to sign")
+	seedPath := flag.String("s", "", "File containing private key seed in hex")
+	flag.Usage = usage
+
+	flag.Parse()
+
+	if *messagePath == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if *seedPath == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	message, err := os.ReadFile(*messagePath)
+	if err != nil {
+		fmt.Printf("couldn't read file: %v\n", err)
+		os.Exit(1)
+	}
+
+	seedHex, err := os.ReadFile(*seedPath)
+	if err != nil {
+		fmt.Printf("couldn't read file: %v\n", err)
+		os.Exit(1)
+	}
+	if len(seedHex) < 64 {
+		fmt.Printf("Expected seed length: 64, got %d\n", len(seedHex))
+		os.Exit(1)
+	}
+
+	var seed [32]byte
+	seedLen, err := hex.Decode(seed[:], seedHex[:64])
+	if err != nil {
+		fmt.Printf("Invalid seed: %s\n", seed)
+		os.Exit(1)
+	}
+	if seedLen != 32 {
+		fmt.Printf("Expected seed length: 32, got %d\n", seedLen)
+		os.Exit(1)
+	}
+
+	privateKey := ed25519.NewKeyFromSeed(seed[:])
+
+	digest := blake2s.Sum256(message)
+	rawSig := [ed25519.SignatureSize]byte(
+		ed25519.Sign(privateKey, digest[:]))
+
+	sig := signature{
+		Alg:    [2]byte{'E', 'b'},
+		KeyNum: [8]byte{1, 7},
+		Sig:    [64]byte{},
+	}
+
+	copy(sig.Sig[:], rawSig[:])
+
+	err = writeBase64(*messagePath+".sig", sig, "", true)
+	if err != nil {
+		fmt.Printf("Couldn't store signature: %v", err)
+		os.Exit(1)
+	}
+}
